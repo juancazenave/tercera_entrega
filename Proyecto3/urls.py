5bed0e6db198a7268f93c24b220536a637b45a38
JuanFCazenave <juanfranciscocazenave@gmail.com>
2023-02-13 11:27:48 -0300

    Actualizacion 13022023

--- Proyecto3/urls.py
+++ Proyecto3/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from .views import saludo, segunda_vista, diadehoy, mi_nombre_es, probando_template
-from AppProyecto3.views import curso
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', saludo),
-    path('segundavista/', segunda_vista),
-    path('dia-de-hoy/', diadehoy),
-    path('mi-nombre-es/<nombre>', mi_nombre_es),
-    path('probando-template/', probando_template),
-    path('curso/', curso)
+    path('appproyecto3/', include('AppProyecto3.urls'))
 ]
--- Proyecto3/views.py
+++ Proyecto3/views.py
@@ -3,34 +3,34 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 
-def saludo(request):
-    return HttpResponse("Hola Ruti")
+# def inicio(request):
+#     return HttpResponse("VISTA INICIO")
 
-def segunda_vista(request):
-    return HttpResponse("Segunda Vista")
+# def segunda_vista(request):
+#     return HttpResponse("Segunda Vista")
 
-def diadehoy(request):
-    fechahoy = datetime.now()
-    texto = f'Hoy es {fechahoy.strftime("%d/%m/%Y")}'
-    return HttpResponse(texto)
+# def diadehoy(request):
+#     fechahoy = datetime.now()
+#     texto = f'Hoy es {fechahoy.strftime("%d/%m/%Y")}'
+#     return HttpResponse(texto)
 
-def mi_nombre_es(request, nombre):
-    texto = f'Mi nombre es {nombre}'
-    return HttpResponse(texto)
+# def mi_nombre_es(request, nombre):
+#     texto = f'Mi nombre es {nombre}'
+#     return HttpResponse(texto)
 
-def probando_template(request):
-    #mi_html = open('C:/Users/User/Desktop/tercera entrega/Proyecto3/templates/template1.html')
-    #Creo las variables que le voy a pasar al Template
-    nombre = 'Juan'
-    apellido = 'Cazenave'
-    fechadehoy = datetime.now()
-    lista_de_notas = [1, 3, 4, 9, 2]
-    #Creo un diccionario para agrupar las variables que le paso al Template
-    diccionario = {'nombre': nombre, 'apellido': apellido, 'fecha': fechadehoy, 'notas': lista_de_notas}
-    #plantilla = Template(mi_html.read())
-    #mi_html.close()
-    plantilla = loader.get_template('template1.html')
-    #contexto = Context(diccionario)
-    documento = plantilla.render(diccionario)
-    return HttpResponse(documento)
+# def probando_template(request):
+#     #mi_html = open('C:/Users/User/Desktop/tercera entrega/Proyecto3/templates/template1.html')
+#     #Creo las variables que le voy a pasar al Template
+#     nombre = 'Juan'
+#     apellido = 'Cazenave'
+#     fechadehoy = datetime.now()
+#     lista_de_notas = [1, 3, 4, 9, 2]
+#     #Creo un diccionario para agrupar las variables que le paso al Template
+#     diccionario = {'nombre': nombre, 'apellido': apellido, 'fecha': fechadehoy, 'notas': lista_de_notas}
+#     #plantilla = Template(mi_html.read())
+#     #mi_html.close()
+#     plantilla = loader.get_template('template1.html')
+#     #contexto = Context(diccionario)
+#     documento = plantilla.render(diccionario)
+#     return HttpResponse(documento)
 
